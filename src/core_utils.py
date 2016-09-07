7b3b8aaeee64ed4b6af8f5e75251e7c6b11a99ca
abunuwas <joseharoperalta@gmail.com>
2016-09-07 17:58:35 +0100

    G2-161: Add default values for filtering params

--- src/core_utils.py
+++ src/core_utils.py
@@ -1,7 +1,9 @@
 def filter_arg(value, filter_params):
 	return all(value != param for param in filter_params)
 
-def filter_args(args, filter_params):
+def filter_args(args, filter_params=None):
+	if filter_params is None:
+		filter_params = ['', None]
 	return dict((key, value) for key, value in args.items() if filter_arg(value, filter_params))
 
 #args = {'memory': 100, 'cpu': 200, 'image': 'image', 'container': None, 'definition': ''}
